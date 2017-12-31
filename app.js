c26c5737a4275f456b0680beb2d98a0c7dc127e3
robin <robin@makichan.moe>
2017-12-31 19:14:16 +0900

    프론트엔드 작성

---
+++ app.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+	el: '#vue-root',
+	data: {ds: [0, 0, 0, 0, 0, 0]},
+	methods: {
+		refresh() {
+			this.ds = [0, 0, 0, 0, 0, 0];
+			jQuery.getJSON('https://iutseat.planet.moe', (data) => {
+				console.log(data);
+				this.ds = data;
+			});
+		}
+	},
+	computed: {
+		defaultText() {
+			return this.ds[0] ? (this.ds[1] + ' / ' + this.ds[0]) : '로딩중';
+		},
+		studentText() {
+			return this.ds[2] ? (this.ds[3] + ' / ' + this.ds[2]) : '로딩중';
+		},
+		notebookText() {
+			return this.ds[4] ? (this.ds[5] + ' / ' + this.ds[4]) : '로딩중';
+		},
+	}
+});
+app.refresh();
